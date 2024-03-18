50de77cbb3e8f5f77184b3e82905cce45d40b59c
Vibhu955 <vibhu.us24@gmail.com>
2024-03-18 22:50:30 +0530

    Organsized

--- api.js
+++
@@ -1,7 +0,0 @@
-import ConvertApi from 'convertapi-js'
-
-let convertApi = ConvertApi.auth('LMh25vIfApOJLTgW');
-let params = convertApi.createParams();
-params.add('Files', elFileInput.files);
-params.add('FileName', 'Executive');
-let result = await convertApi.convert('images', 'pdf', params);
